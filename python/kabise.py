bc51901b43054a1a0b92b9400d33ce7b47282c7a
ehsangittt <153359527+ehsangittt@users.noreply.github.com>
2025-05-18 15:53:08 +0330

    Delete python/kabise.py

--- python/kabise.py
+++
@@ -1,7 +0,0 @@
-def is_leap_year(year):
-    if (year % 4 == 3) #or (year % 4 == 0 and year % 100 != 0):
-        print("Sal kabise ast.")
-    else:
-        print("Kabise nist.")
-year = int(input("year? "))
-is_leap_year(year)
